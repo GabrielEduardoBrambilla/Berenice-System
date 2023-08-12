2dfa29fcddbcc28322eec30f5c9c8d7f48b9316b
Brambs <gabriel128.br@gmail.com>
2023-08-12 07:54:39 -0300

    Update

--- main.c
+++ main.c
@@ -4,166 +4,131 @@
 int main()
 {
 
-// Atividade 8
-    int input_ano, verificacao_ano;
-    float verificacao_ano_100, verificacao_ano_400, verificacao_ano_04;
+    // Atividade 8
+    int id_item, qnt_item, qnt_parcela;
+    char opc_pagamento;
+    float valor_item, venda_total, pagamento_recebido, valor_parcela, troco;
 
-    printf("Digite um ano ex: 2023\n");
-    scanf("%d", &input_ano);
-
-    verificacao_ano_100 = input_ano % 100;
-    verificacao_ano_400 = input_ano % 400;
-    verificacao_ano_04 = input_ano % 04;
-
-    if(verificacao_ano_100 == 0 && verificacao_ano_400 == 0){
-        printf("Nao e bissexto\n");
-    }else{
-        if(verificacao_ano_04 == 0 && !verificacao_ano_100 ==0){
-          printf("E bissexto\n");
-        }else{
-            printf("Nao e bissexto\n");
+    printf("Item (código) Nome do Item       Valor (Unidade)\n");
+    printf("1             Pão de Forma       R$ 7,50\n");
+    printf("2             Pão de Centeio     R$ 8,69\n");
+    printf("3             Broa de Milho      R$ 5,00\n");
+    printf("4             Sonho              R$ 4,50\n");
+    printf("5             Tubaína            R$ 3,25\n");
+    printf("Digite o id do item que deseja comprar\n");
+    scanf("%d", &id_item);
+    printf("Quantos itens deseja comprar\n");
+    scanf("%d", &qnt_item);
+    if (id_item == 1 || id_item == 2 || id_item == 3 || id_item == 4 || id_item == 5)
+    {
+        if (id_item == 1)
+        {
+            valor_item = 7.50;
         }
-    }
-
-// Atividade 7
-    int input_num, conta;
-    printf("Digite um valor inteiro\n");
-    scanf("%d", &input_num);
-
-    conta = input_num % 2;
-
-    if( conta == 0){
-        printf("Numero par\n");
-    }
-    else{
-        printf("Numero impar\n");
-    }
-
-
-
-// Atividade 5
-    int input_A, input_B, resultado;
-    printf("Digite dois valores inteiros\n");
-
-    scanf("%d", &input_A);
-
-    scanf("%d", &input_B);
-
-    resultado = (input_A+input_B)*(input_A+input_B);
-    printf("Resultado: %d \n", resultado);
-
-
-
-
-// Atividade 4
-    int input_1, input_2, input_3;
-    int maior = 0, medio = 0, menor =0;
-    printf("Digite tres valores\n");
-
-    scanf("%d ", &input_1);
-
-    scanf("%d", &input_2);
-
-    scanf("%d", &input_3);
-
-    if(input_1 > input_2 && input_1 > input_3){
-        maior = input_1;
-        if(input_2 > input_3){
-            medio = input_2;
-            menor = input_3;
-        }else{
-            medio = input_3;
-            menor = input_2;
+        if (id_item == 2)
+        {
+            valor_item = 8.69;
         }
-    }
-    else{
-        if (input_2 > input_1 && input_2 > input_3){
-            maior= input_2;
-            if(input_1 > input_3){
-                medio = input_1;
-                menor = input_3;
-            }else{
-                medio = input_1;
-                menor = input_2;
-            }
+        if (id_item == 3)
+        {
+            valor_item = 5;
         }
-        else{
-            maior = input_3;
-            if(input_1 > input_2){
-                medio = input_1;
-                menor = input_2;
-            }else{
-                medio = input_2;
-                menor = input_1;
-            }
+        if (id_item == 4)
+        {
+            valor_item = 4.50;
         }
-    }
-
-    printf("Maior: %d \nMedio: %d \nMenor: %d ", maior, medio, menor);
-
-// Atividade 3
-    int horas, minutos, segundos, segundos_input;
-    printf("\n digite a hora desejada: ");
-    scanf("%d", &horas);
-
-    printf("\n digite os minutos desejados: ");
-    scanf("%d", &minutos);
-
-    printf("\n digite os segundos desejados: ");
-    scanf("%d", &segundos_input);
-
-    segundos = (horas*60)*60+(minutos*60)+segundos_input;
-    printf("segundos igual: %d", segundos);
-    return 0;
-
-
-// Atividade 2
-      float media, prova, trabalho;
-
-    printf("\n Insira a nota da sua prova: ");
-    scanf("%f", &prova);
-
-    printf("\n Insira a nota do seu trabalho: ");
-    scanf("%f", &trabalho);
-
-    media = (prova * 0.4) + (trabalho * 0.6); // Média ponderada correta
-
-    printf("\n Valor da prova: %.2f", prova);
-    printf("\n Valor do trabalho: %.2f", trabalho);
-    printf("\n Media: %4.f", media);
-
-    if (media >=70){
-        printf("\n aprovado");
+        if (id_item == 5)
+        {
+            valor_item = 3.25;
+        }
     }
     else
-        if(media <70)
-        printf("\n reprovado");
+    {
+        printf("Id digitado invalido, multa aplicada\n");
+        valor_item = 1000;
+    }
+    venda_total = qnt_item * valor_item;
 
+    system("cls");
+    printf("Deseja pagar a vista ou a prazo digite:\n");
+    printf("A - para avista\n");
+    printf("P - para a prazo\n");
+    scanf(" %c", &opc_pagamento);
+    if (opc_pagamento == 'A' || opc_pagamento == 'a' || opc_pagamento == 'P' || opc_pagamento == 'p')
+    {
+        if (opc_pagamento == 'A' || opc_pagamento == 'a')
+        {
+            printf("\nPagamento avista selecionado\n");
+            if (venda_total <= 50)
+            {
+                // Desconto de 5%
+                venda_total = venda_total - (venda_total * 0.05);
+            }
+            if (venda_total > 50 && venda_total < 100)
+            {
+                // Desconto de 10%
+                venda_total = venda_total - (venda_total * 0.1);
+            }
+            if (venda_total >= 100)
+            {
+                // Desconto de 18%
+                venda_total = venda_total - (venda_total * 0.18);
+            }
+            printf("Digite o valor recebido para calcular o troco\n");
+            printf("Total da venda: RS %f\n", venda_total);
+            scanf("%f", &pagamento_recebido);
+            if (pagamento_recebido == 0 || pagamento_recebido < 0)
+            {
+                printf("Falta receber: RS%f\n", venda_total);
+            }
+            else if (pagamento_recebido < venda_total && pagamento_recebido != 0)
+            {
+                troco = venda_total - pagamento_recebido;
 
-
-// Atividade 1
-    float raio, altura, volume ;
-    printf("\n digite o valor do raio: ");
-    scanf("%f", &raio);
-
-    printf("\n digite o valor da altura: ");
-    scanf("%f", &altura);
-
-    volume = (3,14*(raio*raio)*altura);
-    printf("o valor de volume e: %f", volume);
-
-    if (volume>=1000){
-        printf("\n suficiente");
+                printf("Falta receber: RS%f\n", troco);
+            }
+            else
+            {
+                troco = venda_total - pagamento_recebido;
+                troco = troco * -1;
+                printf("O troco é %f\n", troco);
+            }
+        }
+        else
+        {
+            printf("Digite a quantidade de vezes que deseja parcelar\n");
+            scanf("%d", &qnt_parcela);
+            if (qnt_parcela == 0 || qnt_parcela < 0)
+            {
+                printf("Quantidade de parcelas invalida: \nA receber: RS%f\n", venda_total);
+            }
+            if (qnt_parcela <= 3 && qnt_parcela >= 1)
+            {
+                venda_total = venda_total + (venda_total * 0.05);
+                valor_parcela = venda_total / qnt_parcela;
+                system("cls");
+                printf("Juros Simples: 5%%\n");
+                printf("Parcelado em: X%d\n", qnt_parcela);
+                printf("Total: RS%f\n", venda_total);
+                printf("Valor da parcela: RS%f\n", valor_parcela);
+            }
+            if (qnt_parcela > 3)
+            {
+                venda_total = venda_total + (venda_total * 0.08);
+                // divide o total da venda pela quantidade de parcelas para descobrir o quanto sera recebido por mes
+                valor_parcela = venda_total / qnt_parcela;
+                system("cls");
+                printf("Juros Simples: 8%%\n");
+                printf("Parcelado em: X%d\n", qnt_parcela);
+                printf("Total: RS%f\n", venda_total);
+                printf("Valor da parcela: RS%f\n", valor_parcela);
+            }
+        }
     }
     else
-        if(volume <1000){
-    printf("\n insuficiente");
+    {
+        printf("Opção invalida");
     }
 
-
     return 0;
-
-
-
-
 }
